57b314805cb8a3a09791c68d982af3c43c0d1686
maxus <maxus.info.mail@gmail.com>
2022-10-13 18:47:35 +0300

    Small test stuff

--- src/tests.py
+++ src/tests.py
@@ -2,16 +2,18 @@ from unittest import TestCase, main
 from api import Controller
 from hashlib import sha256
 
+CURRENT_NGROK_URL = "https://9ad0-2a00-f940-2-4-2-00-4047.eu.ngrok.io"
+
 class APITests(TestCase):
     def test_user_data(self):
         user_hash = sha256(b"Hello, World!").hexdigest()
-        controller = Controller("https://9ad0-2a00-f940-2-4-2-00-4047.eu.ngrok.io")
+        controller = Controller(CURRENT_NGROK_URL)
         resp = controller.user_data(user_hash)
         self.assertEqual(user_hash, resp.card_hash)
 
     def test_register(self):
         user_hash = sha256(b"Hello, World!").hexdigest()
-        controller = Controller("https://9ad0-2a00-f940-2-4-2-00-4047.eu.ngrok.io")
+        controller = Controller(CURRENT_NGROK_URL)
         resp = controller.register(user_hash)
         # none, because already registered
         self.assertIsNone(resp)
